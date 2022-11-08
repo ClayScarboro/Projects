6f83645e655ee4af6adc0c17ed67d494f811ad0d
ClayScarboro <113311871+ClayScarboro@users.noreply.github.com>
2022-11-08 12:42:15 -0500

    Update sim_bp.cc

--- read-trace/cpp_files/sim_bp.cc
+++ read-trace/cpp_files/sim_bp.cc
@@ -106,6 +106,7 @@ int main (int argc, char* argv[])
     }
     
     branchPredictor mainBP(mUse,nUse);
+    printf("Success1\n");
     
     char str[2];
     while(fscanf(FP, "%lx %s", &addr, str) != EOF)
